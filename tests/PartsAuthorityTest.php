ae438396281888a0afd07900cd22e0aacdfa9012
Mahmudur Rahman <mahmud@mazegeek.com>
2019-12-13 15:51:19 +0600

    Add test support

---
+++ tests/PartsAuthorityTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by MD. Mahmud Ur Rahman <mahmudkuet11@gmail.com>.
+ */
+
+namespace Mahmud\PartsAuthority\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PartsAuthorityTest extends TestCase {
+    /**
+     * @test
+     */
+    public function example_test() {
+        $this->assertTrue(true);
+    }
+}
